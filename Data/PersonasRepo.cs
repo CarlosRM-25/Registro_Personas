830c20c86a39de3efda81efe5ea667b34ac26435
CarlosRM-25 <158112977+CarlosRM-25@users.noreply.github.com>
2025-12-07 14:20:01 -0600

    Add files via upload

---
+++ Data/PersonasRepo.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_Personas.Models;
+
+namespace Registro_Personas.Data
+{
+    // Solución: Heredar de DbContext para que PersonasRepo sea un contexto válido
+    public class PersonasRepo : DbContext
+    {
+        public PersonasRepo(DbContextOptions<PersonasRepo> options)
+            : base(options) { }
+
+        public DbSet<Personas> Personas { get; set; } = null!;
+    }
+}
